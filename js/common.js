36e68b8432260f0612879116fac02a769cffb28c
Jonathan Fuerth <jfuerth@gmail.com>
2013-06-26 16:26:35 -0400

    Replaced jQuery plugin that Twitter broke with Twitter's own timeline widget
    'cuz that's what they wanted...

--- js/common.js
+++ js/common.js
@@ -4,8 +4,10 @@
 	autoplay    : true,
 	 interval    : 6000 
   });
+
   // lightbox gallery
   $(".gallery:first a[rel^='prettyPhoto']").prettyPhoto({animation_speed:'normal',theme:'light_square',slideshow:3000, autoplay_slideshow: true});
+
   // social sharing
   $('#shareme').sharrre({
 	  share: {
@@ -22,16 +24,3 @@
 	  enableCounter: true,
 	  enableTracking: true
 	});
-	// twitter feed
-	$(".tweet").tweet({
-            username: "jbosserrai",
-            join_text: "auto",
-            avatar_size: 32,
-            count: 4,
-            auto_join_text_default: " ", 
-            auto_join_text_ed: " ",
-            auto_join_text_ing: " ",
-            auto_join_text_reply: " ",
-            auto_join_text_url: " ",
-            loading_text: "loading tweets..."
-        });
