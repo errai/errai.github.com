ba484616a53f699829a8c508168b508f09dbd5f0
Jonathan Fuerth <jfuerth@gmail.com>
2013-06-28 17:46:30 -0400

    Swapped out sharrre for the actual Twitter and G+ share buttons

--- js/common.js
+++ js/common.js
@@ -9,18 +9,3 @@
   $(".gallery:first a[rel^='prettyPhoto']").prettyPhoto({animation_speed:'normal',theme:'light_square',slideshow:3000, autoplay_slideshow: true});
 
   // social sharing
-  $('#shareme').sharrre({
-	  share: {
-		googlePlus: true,
-        twitter: true,
-        facebook: false,
-		pinterest: false
-	  },
-	  buttons: {
-		googlePlus: {size: 'tall'},
-		twitter: {count: 'vertical'}
-	  },
-	  enableHover: true,
-	  enableCounter: true,
-	  enableTracking: true
-	});
